28aff5c70e5986586cba58418fa54d047a91d9f0
JuliaCastro-dev <juliaprofessional21@gmail.com>
2023-01-24 10:11:52 -0300

    fix: add scrollview on home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { generateFromYearBeginning } from "../utils/generate-from-year-beginning";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const datesFromYearStart = generateFromYearBeginning();
-
+const minimumSummaryDatesSize = 18 * 5; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length;
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16  ">
@@ -21,11 +22,24 @@ export function Home() {
           </Text>
         ))}
       </View>
-      <View className="flex-row flex-wrap">
-        {datesFromYearStart.map((date) => (
-          <HabitDay key={date.toISOString()} />
-        ))}
-      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+              <View
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                key={i}
+              ></View>
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
